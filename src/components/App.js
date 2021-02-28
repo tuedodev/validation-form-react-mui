f68727e23a3051e9e947f82fcc4c7fca6cf15e49
Markus Felder <markus.felder@tuedo.de>
2021-02-28 10:36:47 +0100

    CSS changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{display: 'flex', height:'100vh', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-      <p style={{fontSize: '1.6rem', fontWeight: 'bold'}}>Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</p>
+      <p style={{fontSize: '1.6rem', fontWeight: 'bold', marginBottom: '2.6rem'}}>Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</p>
       <p style={{fontSize: '1.15rem', fontWeight: 'normal'}}>Antoine de Saint-Exupéry</p>
     </div>
   );
